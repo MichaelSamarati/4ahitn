2472bc4c43621fc24bfd867fd5ed0d8a3af0f1c3
MichaelSamarati <michael.samarati@gmail.com>
2023-03-28 12:32:23 +0200

    👻

--- src/app/tab2/tab2-routing.module.ts
+++ src/app/tab2/tab2-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
   {
     path: '',
     component: Tab2Page,
+  },
+  {
+    path: 'profile/:id',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   }
 ];
 
